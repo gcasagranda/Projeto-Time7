cca1a1bfd8520f45248e9c1eb9493160159d9e55
Anderson Pereira <apb2627@gmail.com>
2023-05-23 00:19:51 -0300

    first commit

---
+++ FrontEnd/team-7/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+      <h1>Build home page here!</h1>
+  );
+}
---
+++ FrontEnd/team-7/src/app/settings/page.tsx
@@ -0,0 +1,5 @@
+export default function Login() {
+    return (
+        <h1>Build settings page here!</h1>
+    );
+}
