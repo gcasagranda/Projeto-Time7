1ce717dc38b1e7ec86dfde57fec3804c27b38535
Anderson Pereira <apb2627@gmail.com>
2023-06-05 20:18:11 -0300

    login/cadastro

--- FrontEnd/team-7/src/app/login/page.tsx
+++ FrontEnd/team-7/src/app/login/page.tsx
@@ -1,5 +1,20 @@
-export default function Login() {
-    return (
-        <h1>Build login page here!</h1>
-    );
+import Link from 'next/link'
+import styles from '../styles/Login.module.css'
+import LoginCard from './components/LoginCard/loginCard'
+import Input from './components/LoginCard/input/input'
+import Button from './components/LoginCard/button/button'
+
+export default function LoginPage() {
+    return(
+        <div className={styles.background}>
+            <LoginCard title="Entre em sua conta">
+                <form className={styles.form}>
+                <center><Input type="email" placeholder="login" /></center>
+                <center><Input type="password" placeholder="senha" /></center>
+                <center><Button>Acessar</Button></center> 
+                <Link href="/login/signup">Ainda não possui conta?</Link> 
+                </form>
+            </LoginCard>
+        </div>
+    )
 }
